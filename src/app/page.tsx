abcbc90442cd5218318e87135efe32fad437223b
Mateusz Zwierzchowski <zwierzchowski.mateo@gmail.com>
2025-02-10 22:34:00 +0100

    typeanimation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
+'use client'
+import { TypeAnimation } from "react-type-animation";
+
+
 const Home = () => {
     return ( 
         <div className="w-screen h-[87vh] flex justify-center items-center p-4">
-            <h1 className="text-black text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center">
-                Witamy na Wojewódzkim Konkursie Informatycznym Dla Uczniów Klas VII - VIII
-            </h1>
-            
+            <TypeAnimation
+                sequence={[
+                    'Witamy na Wojewódzkim Konkursie Informatycznym Dla Uczniów Klas VII - VIII.',
+                    1000, 
+                    'Organizowanym przez Centrum Kształcenia Zawodowego i Ustawicznego w Łodzi.',
+                    1000,
+
+                ]}
+                wrapper="span"
+                speed={50}
+                className="tekstRuszajacy"
+                repeat={Infinity}
+                />
         </div>
     );
 }
